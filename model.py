d07d725ee44de95275146d5fe208a85b91d95a8d
Lhhiep-maxcode <22028295@vnu.edu.vn>
2024-08-29 16:18:19 +0700

    Change achitecture of yolo model using fully convolutional head

--- model.py
+++ model.py
@@ -1,38 +1,7 @@
-"""
-Implementation of Yolo (v1) architecture
-with slight modification with added BatchNorm.
-"""
-
 import torch
 import torch.nn as nn
-
-""" 
-Information about architecture config:
-Tuple is structured by (kernel_size, filters, stride, padding) 
-"M" is simply maxpooling with stride 2x2 and kernel 2x2
-List is structured by tuples and lastly int with number of repeats
-"""
-
-architecture_config = [
-    (7, 64, 2, 3),
-    "M",
-    (3, 192, 1, 1),
-    "M",
-    (1, 128, 1, 0),
-    (3, 256, 1, 1),
-    (1, 256, 1, 0),
-    (3, 512, 1, 1),
-    "M",
-    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
-    (1, 512, 1, 0),
-    (3, 1024, 1, 1),
-    "M",
-    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
-    (3, 1024, 1, 1),
-    (3, 1024, 2, 1),
-    (3, 1024, 1, 1),
-    (3, 1024, 1, 1),
-]
+from torchvision.models import resnet50, ResNet50_Weights
+from torchsummary import summary
 
 
 class CNNBlock(nn.Module):
@@ -47,69 +16,35 @@ class CNNBlock(nn.Module):
 
 
 class Yolov1(nn.Module):
-    def __init__(self, in_channels=3, **kwargs):
+    def __init__(self, in_channels=1, split_size=10, num_boxes=2, num_classes=1):
         super(Yolov1, self).__init__()
-        self.architecture = architecture_config
-        self.in_channels = in_channels
-        self.darknet = self._create_conv_layers(self.architecture)
-        self.fcs = self._create_fcs(**kwargs)
+        net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        original_conv1 = net.conv1
+        net.conv1 = nn.Conv2d(
+            in_channels=1, 
+            out_channels=original_conv1.out_channels,
+            kernel_size=original_conv1.kernel_size,
+            stride=original_conv1.stride,
+            padding=original_conv1.padding,
+            bias=original_conv1.bias is not None
+        )
+        self.S = split_size
+        self.B = num_boxes
+        self.C = num_classes
+        modules = list(net.children())
+        self.backbone = nn.Sequential(*modules[:-2])
+        print(1)
+        self.head = nn.Sequential(
+            CNNBlock(in_channels=2048, out_channels=1024, kernel_size=1, padding=0, stride=1),  # (20, 20, 1024)
+            CNNBlock(in_channels=1024, out_channels=512, kernel_size=1, padding=0, stride=1),  # (20, 20, 512)
+            CNNBlock(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=2),   # (10, 10, 512)
+            CNNBlock(in_channels=512, out_channels=256, kernel_size=3, padding=1, stride=1),    # (10, 10, 256)
+            nn.Conv2d(in_channels=256, out_channels=11, kernel_size=1, padding=0, stride=1),    # (10, 10, 11)
+        )
 
     def forward(self, x):
-        x = self.darknet(x)
-        return self.fcs(torch.flatten(x, start_dim=1))
-
-    def _create_conv_layers(self, architecture):
-        layers = []
-        in_channels = self.in_channels
-
-        for x in architecture:
-            if type(x) == tuple:
-                layers += [
-                    CNNBlock(
-                        in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3],
-                    )
-                ]
-                in_channels = x[1]
-
-            elif type(x) == str:
-                layers += [nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))]
-
-            elif type(x) == list:
-                conv1 = x[0]
-                conv2 = x[1]
-                num_repeats = x[2]
-
-                for _ in range(num_repeats):
-                    layers += [
-                        CNNBlock(
-                            in_channels,
-                            conv1[1],
-                            kernel_size=conv1[0],
-                            stride=conv1[2],
-                            padding=conv1[3],
-                        )
-                    ]
-                    layers += [
-                        CNNBlock(
-                            conv1[1],
-                            conv2[1],
-                            kernel_size=conv2[0],
-                            stride=conv2[2],
-                            padding=conv2[3],
-                        )
-                    ]
-                    in_channels = conv2[1]
-
-        return nn.Sequential(*layers)
-
-    def _create_fcs(self, split_size, num_boxes, num_classes):
-        S, B, C = split_size, num_boxes, num_classes
-
-        return nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024 * S * S, 4096),
-            nn.Dropout(0.0),
-            nn.LeakyReLU(0.1),
-            nn.Linear(4096, S * S * (C + B * 5)),
-        )
+        x = self.head(self.backbone(x))
+        return x
+    
+    
     
